cea574b3e9ea482227c8ae94ea1f1689e46cc014
chadrakdev <chadrak@outlook.com>
2024-09-29 09:31:56 +0100

    Refactor tasks to interface for extensibility

--- src/types/tasks/task.ts
+++ src/types/tasks/task.ts
@@ -1,11 +1,15 @@
-export type Task = {
-    id: number,
-    title: string,
-    description: string,
-    tags: string,
-    dateAdded: Date,
-    isCompleted: boolean,
-    dateCompleted: Date,
-    isRepeating: boolean,
-    priority: number
+export interface TaskEntity {
+    id: number;
+    title: string;
+    description: string;
+    tags: string;
+    dateAdded: Date;
+    isCompleted: boolean;
+    dateCompleted: Date;
+    isRepeating: boolean;
+    priority: number;
 }
+
+export type CreateTaskDTO = Omit<TaskEntity, 'dateCompleted'>;
+
+export type UpdateTaskDTO = Partial<TaskEntity> & { id: number };
